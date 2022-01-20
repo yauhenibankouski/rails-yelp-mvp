971b9cda49104ecd67000974d93f247eba0a7828
Yauheni Bankouski <yauheni.bankouski@gmail.com>
2022-01-20 09:14:16 +0100

    done almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # #get restaurants form
+  # get "restaurants/new", to: "restaurants#new"
+  # #index all
+  # get "/restaurants", to: "restaurants#index"
+  # #show specific restaurant
+  # get "restaurants/:id", to: "restaurants#show"
+  # #post a new restaurant
+  # post "restaurants", to: "restaurants#create"
+  resources :restaurants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+  )
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
